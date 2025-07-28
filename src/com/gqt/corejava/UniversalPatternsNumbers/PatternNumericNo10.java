85fb9defc895b7d4b1fbfcab7aa8911c086a3ad9
Abhilash Reddy <India@DESKTOP-CUSS6VF>
2025-07-28 18:05:11 +0530

    Adding combination of numbers example

---
+++ src/com/gqt/corejava/UniversalPatternsNumbers/PatternNumericNo10.java
@@ -0,0 +1,48 @@
+/**
+ * 10
+ */
+package com.gqt.corejava.UniversalPatternsNumbers;
+
+import java.util.Scanner;
+
+/**
+ * @author Abhilash Reddy
+ * @category Universal Pattern
+ * @description Printing the Pattern Number 10
+ */
+public class PatternNumericNo10 {
+
+	/**
+	 * @param args
+	 * @description This source code is for Printing Pattern Number 10
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("This program is to print Numeric No 10");
+		System.out.println("Enter the size");
+		int n = sc.nextInt();
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n; j++) {
+				if((i+j==(n/2) && i==(n/4)) || i==(n-1) || j==(n/2)) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.print("\t");
+			for(int j = 0; j<n; j++) {
+				if(j==0 || i==0 || i==(n-1) || j==(n-1)) {
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+
+}
+
